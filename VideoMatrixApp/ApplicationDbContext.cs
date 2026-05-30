3d50e139c4aa9c6e79bf5d092a41668ab87c45e3
daniel <pedrodaniel.fernandezguerrero@alum.uca.es>
2026-05-30 16:25:11 +0200

    ApplicationDbContext

---
+++ VideoMatrixApp/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VideoMatrixApp
+{
+    public class ApplicationDbContext: DbContext
+    {
+        //Constructor 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //Por si tengo que configurar la bd manualmente en caso de que hiciera falta
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        //Referencia a cada tabla de la bd. Igual que en el tutorial pero con mas entidades
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<Connection> Connections { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+        public DbSet<ProfileConnection> ProfileConnections { get; set; }
+
+        
+    }
+}

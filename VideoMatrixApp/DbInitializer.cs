02548f40b4362d530e28b33ec3388e968a9e576b
daniel <pedrodaniel.fernandezguerrero@alum.uca.es>
2026-05-30 18:06:16 +0200

    DbInitializer para rellenar la bd automaticamente

---
+++ VideoMatrixApp/DbInitializer.cs
@@ -0,0 +1,142 @@
+﻿namespace VideoMatrixApp
+{
+    public static class DbInitializer
+    {
+        public static void Seed(ApplicationDbContext context)
+        {
+            // Si ya hay dispositivos, no hacemos nada
+            if (context.Devices.Any())
+                return;
+
+            // Transmisores
+            var tx1 = new Device
+            {
+                Name = "Camera 1",
+                IpAddress = "192.168.1.10",
+                Status = DeviceStatus.On,
+                DeviceType = DeviceType.Transmitter,
+                ImageUrl = "https://picsum.photos/300/200?1"
+            };
+
+            var tx2 = new Device
+            {
+                Name = "Gaming PC",
+                IpAddress = "192.168.1.11",
+                Status = DeviceStatus.On,
+                DeviceType = DeviceType.Transmitter,
+                ImageUrl = "https://picsum.photos/300/200?2"
+            };
+
+            var tx3 = new Device
+            {
+                Name = "Gaming PC",
+                IpAddress = "192.168.1.12",
+                Status = DeviceStatus.On,
+                DeviceType = DeviceType.Transmitter,
+                ImageUrl = "https://picsum.photos/seed/camera1/300/200"
+            };
+
+            var tx4 = new Device
+            {
+                Name = "Gaming PC",
+                IpAddress = "192.168.1.13",
+                Status = DeviceStatus.Off,
+                DeviceType = DeviceType.Transmitter,
+                ImageUrl = "https://picsum.photos/seed/studio2/300/200"
+            };
+
+            var tx5 = new Device
+            {
+                Name = "Gaming PC",
+                IpAddress = "192.168.1.14",
+                Status = DeviceStatus.Standby,
+                DeviceType = DeviceType.Transmitter,
+                ImageUrl = "https://picsum.photos/seed/broadcast3/300/200"
+            };
+
+            
+            // Receptores
+            var rx1 = new Device
+            {
+                Name = "Screen 1",
+                IpAddress = "192.168.1.20",
+                Status = DeviceStatus.On,
+                DeviceType = DeviceType.Receiver,
+                ImageUrl = ""
+            };
+
+            var rx2 = new Device
+            {
+                Name = "Projector",
+                IpAddress = "192.168.1.21",
+                Status = DeviceStatus.On,
+                DeviceType = DeviceType.Receiver,
+                ImageUrl = ""
+            };
+
+            var rx3 = new Device
+            {
+                Name = "Projector",
+                IpAddress = "192.168.1.22",
+                Status = DeviceStatus.On,
+                DeviceType = DeviceType.Receiver,
+                ImageUrl = ""
+            };
+
+
+            var rx4 = new Device
+            {
+                Name = "Projector",
+                IpAddress = "192.168.1.23",
+                Status = DeviceStatus.On,
+                DeviceType = DeviceType.Receiver,
+                ImageUrl = ""
+            };
+
+            var rx5 = new Device
+            {
+                Name = "Projector",
+                IpAddress = "192.168.1.24",
+                Status = DeviceStatus.On,
+                DeviceType = DeviceType.Receiver,
+                ImageUrl = ""
+            };
+
+            //Guarda los dispositivos en la bd 
+            context.Devices.AddRange(tx1, tx2, tx3, tx4, tx5, rx1, rx2, rx3, rx4, rx5);
+
+            context.SaveChanges();
+
+            // Conexiones actuales
+            context.Connections.AddRange(
+                new Connection      //pantalla 1 - camara 1
+                {
+                    ReceiverId = rx1.Id,
+                    TransmitterId = tx1.Id
+                },
+                new Connection  
+                {
+                    ReceiverId = rx2.Id,
+                    TransmitterId = tx2.Id
+                },
+                new Connection  
+                {
+                    ReceiverId = rx4.Id,
+                    TransmitterId = tx1.Id
+                },
+                new Connection  
+                {
+                    ReceiverId = rx3.Id,
+                    TransmitterId = tx2.Id
+                },
+                new Connection  
+                {
+                    ReceiverId = rx5.Id,
+                    TransmitterId = tx4.Id
+                }
+            );
+
+            context.SaveChanges();
+        }
+    }
+}
